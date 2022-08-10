d7d94583a79480afac6ac2959c6fd882f06faeca
Vap0r1ze <superdash993@gmail.com>
2022-08-10 01:37:18 -0400

    use web bundle

--- packages/shiki/rollup.config.js
+++ packages/shiki/rollup.config.js
@@ -4,6 +4,8 @@ import dts from 'rollup-plugin-dts'
 import typescript from 'rollup-plugin-typescript2'
 import copy from 'rollup-plugin-copy'
 import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import { version } from './package.json'
 
 const external = ['vscode-oniguruma', 'vscode-textmate']
 
@@ -29,6 +31,62 @@ export default [
       typescript(),
       nodeResolve(),
       commonjs()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.browser.mjs',
+        format: 'esm',
+        plugins: [
+          replace({
+            __CDN_ROOT__: ''
+          })
+        ]
+      },
+      {
+        file: 'dist/index.iife.js',
+        format: 'iife',
+        name: 'shiki',
+        extend: true,
+        plugins: [
+          replace({
+            __CDN_ROOT__: ''
+          })
+        ]
+      },
+      {
+        file: 'dist/index.unpkg.iife.js',
+        format: 'iife',
+        name: 'shiki',
+        extend: true,
+        plugins: [
+          replace({
+            __CDN_ROOT__: `https://unpkg.com/vpc-shiki-fork@${version}/`
+          })
+        ]
+      },
+      {
+        file: 'dist/index.jsdelivr.iife.js',
+        format: 'iife',
+        name: 'shiki',
+        extend: true,
+        plugins: [
+          replace({
+            __CDN_ROOT__: `https://cdn.jsdelivr.net/npm/shiki@${version}/`
+          })
+        ]
+      }
+    ],
+    plugins: [
+      replace({
+        __BROWSER__: JSON.stringify(true)
+      }),
+      typescript(),
+      nodeResolve(),
+      commonjs(),
+      terser()
     ]
   },
   {
